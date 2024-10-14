a889c59862d1ee4bab65e899f920037cf35f711c
Vadkon07 <vadkonbeats@gmail.com>
2024-10-14 22:01:37 +0000

    Small improvements

--- main.asm
+++ main.asm
@@ -1,5 +1,6 @@
 section .data
-	msg db "Hello, World!", 0xA
+	msg db "Hello, World!\n", 0xA
+	second db "How are you today?"
 
 section .text
 	global _start
@@ -9,6 +10,12 @@ _start:
 	mov rdi, 1
 	mov rsi, msg
 	mov rdx, 13
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, second
+	mov rdx, 18 
 	syscall
 
 	mov rax, 60
